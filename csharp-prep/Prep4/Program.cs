e7ba658b01dda15d26cc53fb58c0a8e1c6eac798
Splinter8900 <bdshurtz@gmail.com>
2023-01-11 15:41:56 -0700

    made a list maker

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,47 @@
 using System;
-
+using System.Collections.Generic;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
-    }
+        List<int> numbers = new List<int>();
+
+        int userNumber = -1;
+        while (userNumber != 0)
+        {
+            Console.Write("Please enter a number. If you would like to quit, enter 0:");
+
+            string userResponse = Console.ReadLine();
+            userNumber = int.Parse(userResponse);
+
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
+
+        int sum = 0;
+            foreach (int number in numbers)
+            {
+                sum += number;
+            }
+
+        Console.WriteLine($"The sum of number is: {sum}");
+
+        float avg = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average of the list is: {avg}");
+
+        int max = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+
+        Console.WriteLine($"The largest number is: {max}");
+    }   
+
+        
 }
