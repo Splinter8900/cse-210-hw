d10985591d6c7594a2888e9b4505d95998eb1e77
Splinter8900 <bdshurtz@gmail.com>
2023-01-17 18:34:11 -0700

    w03 learning activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Firefighter";
+        job1._company = "FiresRUs";
+        job1._startYear = 2009;
+        job1._endYear = 2013;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Chef";
+        job2._company = "FoodsRUs";
+        job2._startYear = 2013;
+        job2._endYear = 2023;
+
+        Resume myResume = new Resume();
+        myResume._name = "Ben Shurtz";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
+
     }
 }
