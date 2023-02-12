fbbe2698ee2c759f2da286a36420897392bc3fd0
Splinter8900 <bdshurtz@gmail.com>
2023-02-11 23:21:59 -0700

    removes scripture collection class

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,8 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        ScriptureCollection scriptures = new ScriptureCollection();
-        Scripture scripture = scriptures.GetRandomScripture();
+        Reference reference = new Reference("D+C", 90, 24);
+        string verse = "Search diligently, pray always, and be believing, and all things shall work together for your good.";
+        Scripture scripture = new Scripture(reference, verse);
         var input = "";
        while (true){
             Console.WriteLine("Welcome to the memoriser app! To play simply hit enter. When you want to quit, type 'Quit'");
