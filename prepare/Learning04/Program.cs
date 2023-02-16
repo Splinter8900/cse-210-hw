7e5cd50c7127a51fa6a9519b1d09c99b7ede0796
Splinter8900 <bdshurtz@gmail.com>
2023-02-15 22:30:44 -0700

    finished Assignment

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment1 = new Assignment("Ben Shurtz", "Physics");
+        Console.WriteLine(assignment1.GetSummary());
+
+        MathAssignment assignment2 = new MathAssignment("Ben Shurtz", "Trig", "3.5", "10,11,12" );
+        Console.WriteLine(assignment2.GetSummary());
+        Console.WriteLine(assignment2.GetHomeworkList());
+
+        WritingAssignment assignment3 = new WritingAssignment("Ben Shurtz","Politics", "Unique government policies");
+        Console.WriteLine(assignment3.GetSummary());
+        Console.WriteLine(assignment3.GetWritingInformation());
+
+       
     }
 }
