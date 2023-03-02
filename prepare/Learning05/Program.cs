8480a0fd4abce587baf0922e62199796705c0f0e
Splinter8900 <bdshurtz@gmail.com>
2023-03-01 22:19:26 -0700

    finished shape maker

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square("blue", 5);
+        shapes.Add(s1);
+
+        Rectangle s2 = new Rectangle("blue", 5, 6);
+        shapes.Add(s2);
+
+        Circle s3 = new Circle("blue", 5);
+        shapes.Add(s3);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+
+            double area = shape.GetArea();
+
+            Console.Write($"The {color} {shape} has an area of {area}.");
+        }
     }
 }
